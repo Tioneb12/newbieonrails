3a65b9a50ecf439d71426dea885c258ecb866520
Ben <benoit.zeller@gmail.com>
2021-10-31 19:01:35 +0100

    feat(routes): add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+=begin
+=end
   root 'home#index'
-  localized do 
-    resources :posts, only: %i[index show]
+  localized do
+    resources :posts, only: %i[index show new create]
     resources :sub_categories, only: %i[index show]
     resources :categories, only: %i[index show]
   end
+
+  scope 'office', module: 'office', as: 'admin' do
+    get '', to: 'dashboard#index', as: '/'
+    resources :posts, except: :show
+  end
+
 end
