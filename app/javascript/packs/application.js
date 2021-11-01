ae44994861cabe5be3a1d8924e44ecaff2caa637
Ben <benoit.zeller@gmail.com>
2021-11-01 12:34:35 +0100

    feat(tailwind): update path

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("../../assets/stylesheets/application.css")
+require("../../assets/stylesheets/application.scss")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
