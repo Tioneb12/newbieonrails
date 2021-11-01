11f6cff89409afa04a93cac1277b66eb3cd8c7d5
Ben <benoit.zeller@gmail.com>
2021-11-01 17:20:19 +0100

    feat(category): add main_image

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
+    has_one_attached :main_image
     has_many :sub_categories, inverse_of: 'category'
 end
