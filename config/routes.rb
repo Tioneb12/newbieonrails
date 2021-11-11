2aec0d4381b94d3280326c2bb6dd740898299d65
Ben <benoit.zeller@gmail.com>
2021-11-11 20:21:48 +0100

    feat(routes): update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,17 @@ Rails.application.routes.draw do
   post 'uploader/image', to: 'uploader#image'
   root 'home#index'
   localized do
-    resources :posts, only: %i[index show new create]
-    resources :sub_categories, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+    end
     resources :categories, only: %i[index show]
   end
 
   scope 'office', module: 'office', as: 'admin' do
     get '', to: 'dashboard#index', as: '/'
     resources :posts, except: :show
+    resources :categories, except: :show
+    resources :sub_categories, except: :show
   end
 
 end
